42b5d024a0bb39171851fb9582eaba103e29c5fb
Admin <maxmudovabdullo97@gmail.com>
2020-05-26 18:01:51 +0500

    Initial commit

---
+++ app/src/main/java/myway/bottombarui/MainActivity.java
@@ -0,0 +1,14 @@
+package myway.bottombarui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
